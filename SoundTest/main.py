a963a9ffea68172b2078d27f1eb90f5ac90fea58
idadorable <ida@thoft.dk>
2022-03-03 08:38:51 +0100

    Preprocessed the sound by normalizing, noice reduction and trimming

--- SoundTest/main.py
+++ SoundTest/main.py
@@ -1,10 +1,13 @@
-from pydub import AudioSegment
+from pydub import AudioSegment, effects
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from tempfile import mktemp
 import numpy as np
 import librosa
 import librosa.display
+import os
+import soundfile as sf
+import noisereduce as nr
 
 
 def convert_audio(audio):
@@ -39,11 +42,33 @@ def plot_specgram_librosa(filename, fileformat, no):
     librosa.display.specshow(librosa.amplitude_to_db(out, ref=np.max), y_axis='log', x_axis='time', sr=sampling_rate)
 
 
-if __name__ == '__main__':
-    plot_specgram_plt('pære_1.m4a', "m4a", 1)
-    plot_specgram_plt('bære_1.m4a', "m4a", 2)
+def preprocess_sound():
+    files = []
+    dir_list = os.listdir()
+    path = "sound/2ndBatch"
+    for file in os.listdir(path):
+        time_series, sampling_rate = librosa.load(path + "/" + file)  # Makes floating point time series
 
-    plot_specgram_librosa('pære_1.m4a', "m4a", 3)
-    plot_specgram_librosa('bære_1.m4a', "m4a", 4)
+        # Normalize (make all batches the same level)
+        max_peak = np.max(np.abs(time_series))
+        ratio = 1 / max_peak
+        time_series = time_series * ratio
+
+        # Reduce noice and trim
+        reduced_noise = nr.reduce_noise(y=time_series, sr=sampling_rate)
+        time_series_trimmed, index = librosa.effects.trim(reduced_noise, top_db=30)
+
+        files.append(time_series_trimmed)
+        #sf.write(str(time_series_trimmed) + '.wav', time_series_trimmed, sampling_rate, subtype='PCM_24')
+    return files
+
+
+if __name__ == '__main__':
+    sound_files = preprocess_sound()
+    #plot_specgram_plt('sound/1stBatch/pære_1.m4a', "m4a", 1)
+    #plot_specgram_plt('sound/1stBatch/bære_1.m4a', "m4a", 2)
+
+    #plot_specgram_librosa('sound/1stBatch/pære_1.m4a', "m4a", 3)
+    #plot_specgram_librosa('sound/1stBatch/bære_1.m4a', "m4a", 4)
 
     plt.show()
