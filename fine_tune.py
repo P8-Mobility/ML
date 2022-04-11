cfeec983d507cfdd132f6010b559840a7b308bcb
Thomas Lorentzen <thomas@tlorentzen.net>
2022-04-11 09:34:41 +0200

    WIP: Housekeeping

--- fine_tune.py
+++ fine_tune.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import pathlib
 import shutil
@@ -21,7 +22,7 @@ def fine_tune(data_dir: str, model_name: str = "paere"):
     pf.prepare_feature(pathlib.Path(data_dir + "/validate"), "uni2005/")
     pt.prepare_token(pathlib.Path(data_dir + "/validate"), "uni2005/", 'dan')
 
-    for epochs in range(30, 70, 5):
+    for epochs in range(30, 75, 5):
         print("Training model with " + str(epochs) + " epochs...")
 
         # Ignore errors is set to true, to avoid exception if the folder does not exist
@@ -29,3 +30,13 @@ def fine_tune(data_dir: str, model_name: str = "paere"):
 
         # command to fine_tune your data
         os.system("python -m allosaurus.allosaurus.bin.adapt_model --pretrained_model=uni2005 --new_model=" + model_name + "_" + str(epochs) + " --path=" + data_dir + " --lang=dan --device_id=-1 --epoch=" + str(epochs))
+
+        # Find all saved states of the model during training for deletion
+        file_list = glob.glob("allosaurus/allosaurus/pretrained/" + model_name + "_" + str(epochs)+"/model_*.pt")
+        print("Housekeeping: removing "+str(len(file_list))+" intermediate state models from training...")
+
+        for filePath in file_list:
+            try:
+                os.remove(filePath)
+            except:
+                print("Error while deleting file : ", filePath)
